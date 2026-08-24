a83c2ff19ad0c35fe7cde0096dc29964579a6eb7
MAX\cmhan <cmhanapp@gmail.com>
2026-08-24 22:29:39 +0900

    [Max] - 08/24일 수업 갱신

---
+++ Source/FPS/Private/DummyParticle.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DummyParticle.h"
+#include "Particles/ParticleSystemComponent.h"
+
+// Sets default values
+ADummyParticle::ADummyParticle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+    staticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("staticMeshComponent"));
+    SetRootComponent(staticMeshComponent);
+
+    particleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("particleSystem"));
+    particleSystem->SetupAttachment(GetRootComponent());
+    particleSystem->SetRelativeLocation(FVector(0.f, 0.f, 600.f));
+}
---
+++ Source/FPS/Public/DummyParticle.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DummyParticle.generated.h"
+
+class UParticleSystemComponent;
+
+UCLASS()
+class FPS_API ADummyParticle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADummyParticle();
+
+private:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "DummyParticle", meta = (AllowPrivateAccess))
+    TObjectPtr<UStaticMeshComponent> staticMeshComponent;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "DummyParticle", meta = (AllowPrivateAccess))
+    TObjectPtr<UParticleSystemComponent> particleSystem;
+
+    UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "DummyParticle", meta = (AllowPrivateAccess))
+    int64 testNumber;
+};
