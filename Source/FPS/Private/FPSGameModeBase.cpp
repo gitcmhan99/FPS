5b0007afdf6b4d7ac0d1e1cdfadf3eb46e79c26f
MAX\cmhan <cmhanapp@gmail.com>
2026-09-01 02:17:01 +0900

    8/31

---
+++ Source/FPS/Private/FPSGameModeBase.cpp
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSGameModeBase.h"
+#include "FPSPlayerControllerBase.h"
+#include "FPSPawnBase.h"
+#include "FPS/FPS.h"
+
+AFPSGameModeBase::AFPSGameModeBase()
+{
+    MYLOG("Start AFPSGameModeBase()");
+
+    //TSubclassOf<APlayerController>
+    PlayerControllerClass = AFPSPlayerControllerBase::StaticClass();
+    //TSubclassOf<APawn>
+    DefaultPawnClass = AFPSPawnBase::StaticClass();
+
+    MYLOG("End AFPSGameModeBase()");
+}
+
+void AFPSGameModeBase::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
+{
+    MYLOG("Start InitGame MapName: %s, Options: %s", *MapName, *Options);
+
+    Super::InitGame(MapName, Options, ErrorMessage);
+
+    MYLOG("End InitGame");
+}
+
+void AFPSGameModeBase::InitGameState()
+{
+    MYLOG("Start InitGameState()");
+
+    Super::InitGameState();
+
+    MYLOG("End InitGameState()");
+}
+
+void AFPSGameModeBase::PreInitializeComponents()
+{
+    Super::PreInitializeComponents();
+}
+
+void AFPSGameModeBase::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+}
+
+
+
+void AFPSGameModeBase::PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
+{
+    Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+}
+
+APlayerController* AFPSGameModeBase::Login(UPlayer* NewPlayer, ENetRole InRemoteRole, const FString& Portal, const FString& Options, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
+{
+    APlayerController* PlayerController = Super::Login(
+        NewPlayer,
+        InRemoteRole,
+        Portal,
+        Options,
+        UniqueId,
+        ErrorMessage
+    );
+
+    return PlayerController;
+}
+
+void AFPSGameModeBase::PostLogin(APlayerController* NewPlayer)
+{
+    Super::PostLogin(NewPlayer);
+}
---
+++ Source/FPS/Public/FPSGameModeBase.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_API AFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+    AFPSGameModeBase();
+
+public:
+    virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
+    virtual void InitGameState() override;
+    virtual void PreInitializeComponents() override;
+    virtual void PostInitializeComponents() override;
+
+    virtual void PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage) override;
+    virtual APlayerController* Login(UPlayer* NewPlayer, ENetRole InRemoteRole, const FString& Portal, const FString& Options, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage) override;
+     virtual void PostLogin(APlayerController* NewPlayer) override;
+};
